8163439a1d321d480339f3c1b807446bb69e8d4d
dpbac <dani.barretto15@gmail.com>
2020-01-16 13:40:48 +0100

    2nd commit

---
+++ scripts/webscraping_within_temptation_lyrics.py
@@ -0,0 +1,145 @@
+# Inputs - for any other artist replace url and path_csv
+
+# urls where to find hyperlinks for lyrics
+
+urls = ['https://songteksten.net/artist/lyrics/320/within-temptation.html',
+       'https://songteksten.net/artist/lyrics/320/within-temptation/page/2.html',
+       'https://songteksten.net/artist/lyrics/320/within-temptation/page/3.html']
+
+# path where to keep the .csv file with the obtained title of lyrics and lyrics 
+
+path_csv = "../data/lyrics_within_temptation.csv"
+
+# retrieve hyperlinks
+
+# importing packages
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def retrieve_hyperlinks(main_url):
+    """ Extract all hyperlinks in 'main_url' and return a list with these hyperlinks """
+    
+    # Packages the request, send the request and catch the response: r
+
+    r = requests.get(main_url)
+
+    # Extracts the response as html: html_doc
+    html_doc = r.text
+
+    # Create a BeautifulSoup object from the HTML: soup
+    soup = BeautifulSoup(html_doc,features ="lxml")
+    
+    # Find all 'a' tags (which define hyperlinks): a_tags
+
+    a_tags = soup.find_all('a')
+    
+    # Create a list with hyperlinks found
+
+    list_links = [link.get('href') for link in a_tags]
+    
+    # Remove none values if there is some
+    
+    list_links = list(filter(None, list_links)) 
+    
+    return list_links
+
+# retrieving all hyperlinks
+
+list_links_lyrics_songteksten_net = []
+
+for url in urls:
+    list_links_lyrics_songteksten_net.extend(retrieve_hyperlinks(url))
+    
+# removing possible duplicates
+list_links_lyrics_songteksten_net = list(set(list_links_lyrics_songteksten_net))
+
+    
+# print('Number of links before filtering:', len(list_links_lyrics_songteksten_net))
+
+# filtering hyperlinks which contain lyrics - specific for songteksten.net
+
+# using url address to filter lyrics
+
+spliting = urls[0].split('/')
+filter_lyrics = spliting[2]+'/lyric/'+spliting[-2]
+
+list_links_lyrics_songteksten_net = [link for link in list_links_lyrics_songteksten_net if (filter_lyrics 
+                                                                              in link) ]
+
+# print('Number of links after filtering:', len(list_links_lyrics_songteksten_net))
+
+# Extract lyrics from hyperlinks
+
+def extract_lyric_from_url(url_lyric):
+    """ Extract lyrics after prettify beautiful soup from www.songteksten.nl """
+    
+    
+    # send a http request
+    r_lyric = requests.get(url_lyric)
+    
+    # obtain text with html containt of the url
+    html_doc_lyric = r_lyric.text
+    
+    # making html easier to read
+    soup_lyric = BeautifulSoup(html_doc_lyric,features ="lxml")
+
+    
+    # prettifying it
+    soup_lyric_pretty = soup_lyric.prettify()
+    
+    # Isolating deal that contains the lyric
+    
+    text = soup_lyric_pretty.split('</h1>\n')[1].split('<div class="buma-consent" role="alert">')[0]
+
+    # Cleaning text and building a list with it
+    list_lyrics = text.split('<br/>\n')
+    list_lyrics = [item.replace('\n','') for item in list_lyrics]
+    list_lyrics = [item.lstrip().rstrip() for item in list_lyrics]
+    
+    # removing empty elements from the list
+    
+    for item in list_lyrics:
+        if str(item) == '':
+            list_lyrics.remove(item)
+            
+    # this part was added after noticing that at least one lyric was not following the normal pattern
+    
+    if '<div' in list_lyrics[0]:
+        list_lyrics = list_lyrics[1:]
+        
+        
+    # Having the lyrics in string format
+    
+    lyrics = '. '.join(list_lyrics)
+            
+    
+    # returning both list and string
+    
+    return list_lyrics, lyrics
+
+list_lyrics = []
+list_title_lyrics = []
+
+# building lists with titles of lyrics and lyrics
+
+for url_lyric in list_links_lyrics_songteksten_net:
+    
+    list_title_lyrics.append(url_lyric.split('/')[-1].split('.')[-2])
+    list_lyrics.append(extract_lyric_from_url(url_lyric)[1])
+
+
+# Creating a dataframe with song titles and lyrics
+
+import pandas as pd
+df = pd.DataFrame({'song_title': list_title_lyrics,
+                  'lyrics': list_lyrics})
+
+# remove '-' from titles and lower case
+
+df['song_title'] = df['song_title'].apply(lambda x: x.replace('-',' ').lower())
+
+# saving dataframe to .csv
+
+df.to_csv(path_csv, index = False)
