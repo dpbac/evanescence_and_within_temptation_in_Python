e963a308c982e4256d39a869f255a350b598d83b
dpbac <dani.barretto15@gmail.com>
2020-02-14 11:28:48 +0100

    Add settings.py and update gitignore

---
+++ settings.py
@@ -0,0 +1,13 @@
+cid ="" 
+secret = "" 
+
+# maybe redirect uri will be required for some of the commands 
+redirect_uri = ""
+
+# the user id of my account
+username = ""
+
+try:
+    from .private import *
+    except Exception:
+        pass
